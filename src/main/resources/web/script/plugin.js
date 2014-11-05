8ebe345e5ce0824f6783a4c987926ca062415320
Malte Reißig <malte@mikromedia.de>
2014-11-05 12:56:35 +0100

    Adapted to be compatible with DM 4.3

--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -2,11 +2,15 @@ dm4c.add_plugin('dm4.csv.plugin', function() {
 
     // upload and import the file
     function importCsv() {
-        dm4c.get_plugin('de.deepamehta.files').open_upload_dialog('csv', function(file) {
+
+        dm4c.get_plugin('de.deepamehta.files').open_upload_dialog('/files/csv', 
+                function(file) {
+
             var status = dm4c.restc.request('POST', '/csv/import/' +
-                    dm4c.selected_object.uri + '/' + file.topic_id),
-                $message = $('<span>').text(status.message),
-                $infos = $('<ul>')
+                    dm4c.selected_object.uri + '/' + file.topic_id)
+            var $message = $('<span>').text(status.message)
+            var $infos = $('<ul>')
+
             if (status.success) {
                 $message.addClass('success')
                 $.each(status.infos, function(i, info) {
@@ -15,17 +19,21 @@ dm4c.add_plugin('dm4.csv.plugin', function() {
             } else {
                 $message.addClass('error')
             }
+
             dm4c.ui.dialog({
                 id: 'csvImportStatus',
                 title: 'CSV Import Status Report',
                 content: $('<div>').append($message).append($infos)
             }).open()
+
         })
     }
 
     // configure menu and type commands
     dm4c.add_listener('topic_commands', function(topic) {
+
         var commands = []
+
         if (topic.type_uri === 'dm4.core.topic_type') {
             commands.push({
                 is_separator : true,

c13038375612bac8fd89f087ef20f3743698a320
mukil <malte@mikromedia.de>
2020-03-25 01:15:18 +0100

    Added user notifications (#2)

--- src/main/js/plugin.js
+++ src/main/js/plugin.js
@@ -1,4 +1,4 @@
-export default ({store, axios: http}) => ({
+export default ({dm5, store, axios: http, Vue}) => ({
 
   init () {
     store.dispatch("registerUploadHandler", {
@@ -8,7 +8,6 @@ export default ({store, axios: http}) => ({
         console.log("[CSV] upload dialog change selected for upload", fileList)
       },
       success: function(response, file, fileList) {
-        console.log("[CSV] file uploaded successful", response, file, fileList)
         this.$store.dispatch("revealTopicById", response.id)
         this.$notify({
           title: 'CSV File Uploaded', type: 'success'
@@ -28,23 +27,43 @@ export default ({store, axios: http}) => ({
   contextCommands: {
     topic: topic => {
       if (topic.typeUri === 'dmx.files.file') {
-        let isCsvFile = (topic.value.indexOf('.csv') != -1) // Fixme: Do the right thing.
+        // 1) Check configuration
+        let hasTargetTypeConfigured = true
+        dm5.restClient.getTopicRelatedTopics(topic.id, {
+          assocTypeUri: "dmx.csv.file_import", othersTopicTypeUri: "dmx.core.topic_type"})
+                .then(response => {
+                  hasTargetTypeConfigured = (response.length > 0) ? true : false
+                })
+        // 2) Check if file topic ends on .CSV
+        let isCsvFile = (topic.value.indexOf('.csv') !== -1) // Fixme: Do the right thing.
         if (isCsvFile) {
           return [{
             label: 'Import CSV',
             handler: id => {
-              // http.post(`/csv/import/dmx.bookmarks.bookmark/${id}`)
-              http.post(`/csv/import/${id}`)
-              .then(function (response) {
-                console.log("Import Status", response.data);
-                this.$notify.success({
-                  title: "Import Successfull", message: JSON.stringify(response.data)
+              // 3.2) Execute if configuration is _potentially_ correct
+              if (hasTargetTypeConfigured) {
+                http.post(`/csv/import/${id}`)
+                .then(function (response) {
+                  console.log("Import Status", response.data)
+                  Vue.prototype.$notify({
+                    title: "CSV Import Successful",
+                    message: JSON.stringify(response.data.infos),
+                    type: "success"
+                  })
                 })
-                // Vue.prototype.$notify
-              })
-              .catch(function (error) {
-                console.error("Import Error", error);
-              })
+                .catch(function (error) {
+                  console.error("Import Error", error);
+                })
+              } else {
+                // 3.2) Notify about mis-configuration before executing the import
+                Vue.prototype.$notify({
+                  title: "Import operation needs to know the <i>Topic Type</>",
+                  dangerouslyUseHTMLString: true, duration: 10000,
+                  message: "You must relate a <i>Topic Type</i> to <i>"+topic.value+"</i> before importing data. "
+                  + "Create a <i>File Import</i> association to the <i>Topic Type</i> you want to import data to.",
+                  type: "error"
+                })
+              }
             }
           }]
         }
